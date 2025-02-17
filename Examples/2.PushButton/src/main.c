184f8d1dae18b5ca7c81396266ceb1c3a48e6b24
aKaReZa <aKaReZa75@gmail.com>
2025-02-17 17:34:16 +0330

    add Example 2

---
+++ Examples/2.PushButton/src/main.c
@@ -0,0 +1,23 @@
+#include "aKaReZa.h"
+
+#define RGB_R_Config  DDRB
+#define RGB_R_Control PORTB
+#define RGB_R_PIN     3
+
+#define SW1_Config    DDRD
+#define SW1_Status    PIND
+#define SW1_Pin       2
+
+int main(void)
+{
+  GPIO_Config_OUTPUT(RGB_R_Config, RGB_R_PIN);
+  GPIO_Config_INPUT(SW1_Config, SW1_Pin);
+  while(1)
+  {
+    if(bitCheckHigh(SW1_Status, SW1_Pin))
+    {
+      bitWaitLow(SW1_Status, SW1_Pin);
+      bitToggle(RGB_R_Control, RGB_R_PIN);
+    };
+  };
+};
